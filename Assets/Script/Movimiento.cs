b165d4b50d0ede562a5950baa221402e629521af
kevinRodriguezCastro <10208@cifpceuta.es>
2024-02-09 21:16:00 +0100

    a

--- Assets/Script/Movimiento.cs
+++ Assets/Script/Movimiento.cs
@@ -6,6 +6,12 @@ public class Movimiento : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;
     private float horizontal;
+    
+    public float Speed;
+    public float JumpForce;
+    //si esta en el aire
+    bool Grounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +22,22 @@ public class Movimiento : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(Physics2D.Raycast(transform.position, Vector3.down,0.1f)){
+            Grounded = true;
+        }else{
+            Grounded = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && Grounded){
+            Jump();
+        }
     }
+
+    private void Jump(){
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
     void FixedUpdate(){
         Rigidbody2D.velocity = new Vector2(horizontal,Rigidbody2D.velocity.y);
     }
